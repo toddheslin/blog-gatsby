da7238c12fcb7afcf98eb54624529ac402777b69
Todd Heslin <todd@beingremarkable.me>
2016-10-17 22:24:54 +0400

    correct image and categories

--- modules/podcast.js
+++ modules/podcast.js
@@ -28,12 +28,12 @@ var feed = new Podcast({
     itunesSummary: 'Focusing on telling remarkable stories, you will discover tips, techniques and strategies on how to connect with your existing tribe to raise money for projects before you even start them. Episodes will feature interviews with current and existing entrepreneurs who have undertaken projects on crowdfunding platforms such as Kickstarter, Indiegogo, Fundable, Crowdfunder and Rally. Why do some projects receive over 1000x more money than they needed? What happens to successful crowdfunding projects after they ship? Why do some people crowdfund even when they don\'t need the money? Is crowdfunding better than seeking investors for your project? Join the fastest growing community of remarkable entrepreneurs.',
     itunesOwner: { name: 'Todd Heslin', email:'todd@heslin.net.au' },
     itunesExplicit: false,
-    itunesCategory: {
+    itunesCategory: [{
         "text": "Business",
         "subcats": [{
           "text": "Management & Marketing"
         }]
-    },
+    }],
     itunesImage: 'https://beingremarkable.herokuapp.com/public/img/podcast.png'
 });
 
