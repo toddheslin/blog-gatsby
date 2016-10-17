3fa50575dc1ef3eb381a9132a06d2879048341a5
Todd Heslin <todd@beingremarkable.me>
2016-10-17 16:04:19 +0400

    relative URLs

--- modules/podcast.js
+++ modules/podcast.js
@@ -11,9 +11,9 @@ var podcastFeed = require(path.join(__dirname, '../content/data/podcast.json'));
 var feed = new Podcast({
     title: 'The Remarkable Crowdfunding Toddcast: Ideas | Tribes | Funding | Lifestyle',
     description: 'Todd Heslin from the Being Remarkable blog explores the question: How do you inspire your tribe to crowdfund and share your new projects?',
-    feed_url: 'http://beingremarkable.me/podcast.xml',
+    feed_url: '/podcast',
     site_url: 'http://beingremarkable.me',
-    image_url: 'http://beingremarkable.me/public/img/podcast.png',
+    image_url: '/public/img/podcast.png',
     //docs: 'http://beingremarkable.me/rss/docs.html',
     author: 'Todd Heslin',
     managingEditor: 'Todd Heslin',
