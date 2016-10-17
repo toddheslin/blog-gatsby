b139257d95f2a6fd01b0e386b69da281e7b554ff
Todd Heslin <todd@beingremarkable.me>
2016-10-17 21:34:04 +0400

    fixed silly public error

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ ghost({
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(cookieParser());
-  app.use(express.static(path.join(__dirname, 'public')));
+  app.use('/public', express.static(path.join(__dirname, 'public')));
 
   // App entry point
   app.get('/', redirects.home);
